f36622ac967b6fcd2f813bcbf4f11e4f53d2f47d
nukiz <61660487+nukiz@users.noreply.github.com>
2022-11-16 17:58:58 +0200

    EXODUS

---
+++ exodus/Trainer/overlay/arraylist.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static exodus.arraylist;
+using static testui.overlay;
+
+namespace exodus
+{
+    public partial class arraylist : Form
+    {
+        public const string WINDOW_NAME = "Growtopia";
+
+        [DllImport("user32.dll")]
+        public static extern short GetAsyncKeyState(System.Windows.Forms.Keys vKey);
+
+        public static IntPtr handle = FindWindow(null, WINDOW_NAME);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string IpClassName, string IpWindowName);
+
+        [DllImport("user32.dll")]
+        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+
+        public static extern bool GetWindowRect(IntPtr hwnd, out RECT IpRect);
+
+        public static RECT rect;
+
+        public struct RECT
+        {
+            public int left, top, right, bottom;
+        }
+
+       
+
+        public arraylist()
+        {
+            InitializeComponent();
+        }
+
+        private void arraylist_Load(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = false;
+            this.TopMost = true;
+
+            int initialStyle = GetWindowLong(this.Handle, -20);
+            SetWindowLong(this.Handle, -20, initialStyle | 0x8000 | 0x20);
+
+            GetWindowRect(handle, out rect);
+            this.Size = new Size(rect.right - rect.left, rect.bottom - rect.top);
+            this.Left = rect.left;
+            this.Top = rect.top;
+            backgroundWorker1.RunWorkerAsync();
+
+            label10.Hide();
+            label11.Hide();
+            label12.Hide();
+            label13.Hide();
+            label2.Hide();
+            label14.Hide();
+            label15.Hide();
+            label16.Hide();
+            label17.Hide();
+            label18.Hide();
+            label19.Hide();
+            label20.Hide();
+            label21.Hide();
+            label22.Hide();
+            label23.Hide();
+            label24.Hide();
+            label25.Hide();
+            label26.Hide();
+            label27.Hide();
+            label28.Hide();
+            label29.Hide();
+            label3.Hide();
+            label4.Hide();
+            label5.Hide();
+            label6.Hide();
+            label7.Hide();
+            label8.Hide();
+            label9.Hide();
+            label31.Hide();
+            label32.Hide();
+            label35.Hide();
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                GetWindowRect(handle, out rect);
+                this.Size = new Size(rect.right - rect.left, rect.bottom - rect.top);
+                this.Left = rect.left;
+                this.Top = rect.top;
+                Thread.Sleep(100);
+            }
+        }
+
+        private void label18_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label35_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label26_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
