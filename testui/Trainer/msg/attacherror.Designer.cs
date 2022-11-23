32a583ecc3ed269e70e35080c373048dbd3b065c
nukiz <61660487+nukiz@users.noreply.github.com>
2022-11-23 20:11:16 +0200

    developer console stuff - non clean up version
    developer console added some stuff redesigned but most of it not

---
+++ testui/Trainer/msg/attacherror.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace exodus.Trainer.msg
+{
+    partial class attacherror
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Crimson;
+            this.panel1.Location = new System.Drawing.Point(-3, -8);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(758, 10);
+            this.panel1.TabIndex = 2;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.attacherror_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.attacherror_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.attacherror_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Crimson;
+            this.label1.Location = new System.Drawing.Point(7, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 30);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "fatal error";
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.attacherror_MouseDown);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.attacherror_MouseMove);
+            this.label1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.attacherror_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(368, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(41, 41);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "╳";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Crimson;
+            this.label2.Location = new System.Drawing.Point(12, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(258, 21);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "This action failed with code 0x1337!";
+            this.label2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.attacherror_MouseDown);
+            this.label2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.attacherror_MouseMove);
+            this.label2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.attacherror_MouseUp);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Crimson;
+            this.label3.Location = new System.Drawing.Point(12, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(306, 21);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "To resolve this error, please open or attach ";
+            this.label3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.attacherror_MouseDown);
+            this.label3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.attacherror_MouseMove);
+            this.label3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.attacherror_MouseUp);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Crimson;
+            this.label4.Location = new System.Drawing.Point(12, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(338, 21);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Growtopia. If this does not fix the error, join our";
+            this.label4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.attacherror_MouseDown);
+            this.label4.MouseMove += new System.Windows.Forms.MouseEventHandler(this.attacherror_MouseMove);
+            this.label4.MouseUp += new System.Windows.Forms.MouseEventHandler(this.attacherror_MouseUp);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Crimson;
+            this.label5.Location = new System.Drawing.Point(12, 118);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(325, 21);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Discord and ask for help from the developers.";
+            this.label5.MouseDown += new System.Windows.Forms.MouseEventHandler(this.attacherror_MouseDown);
+            this.label5.MouseMove += new System.Windows.Forms.MouseEventHandler(this.attacherror_MouseMove);
+            this.label5.MouseUp += new System.Windows.Forms.MouseEventHandler(this.attacherror_MouseUp);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Crimson;
+            this.panel2.Location = new System.Drawing.Point(-169, 160);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(758, 10);
+            this.panel2.TabIndex = 10;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Crimson;
+            this.panel3.Location = new System.Drawing.Point(413, -16);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(8, 225);
+            this.panel3.TabIndex = 11;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Crimson;
+            this.panel4.Location = new System.Drawing.Point(-6, -2);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(8, 225);
+            this.panel4.TabIndex = 12;
+            // 
+            // attacherror
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(415, 162);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "attacherror";
+            this.Text = "attacherror";
+            this.Load += new System.EventHandler(this.attacherror_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.attacherror_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.attacherror_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.attacherror_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+    }
+}
